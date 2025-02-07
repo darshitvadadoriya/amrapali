7e7380c0327eaf1bb2d352f62c512bf7c2cbdab8
darshitvadadoriya <darshit@sanskartechnolab.com>
2025-02-07 19:18:59 +0530

    update outstanding report and release order

--- amrapali/amrapali/page/balance_sheet_page_r/balance_sheet_page_r.js
+++ amrapali/amrapali/page/balance_sheet_page_r/balance_sheet_page_r.js
@@ -6,11 +6,27 @@ frappe.pages['balance-sheet-page-r'].on_page_load = function(wrapper) {
         single_column: true
     });
 
-    // Create a container for filters
+    // Create containers
     var filter_container = $('<div class="filter-section"></div>').appendTo(page.main);
     var report_container = $('<div class="report-section"></div>').appendTo(page.main);
 
-    // Add fields to filter container
+    // Add filter type selector
+    page.add_field({
+        label: 'Filter Type',
+        fieldtype: 'Select',
+        fieldname: 'filter_type',
+        options: [
+            { value: 'fiscal_year', label: 'Fiscal Year' },
+            { value: 'date_range', label: 'Date Range' }
+        ],
+        default: 'fiscal_year',
+        parent: filter_container,
+        change: function() {
+            toggleFilterFields();
+        }
+    });
+
+    // Add company field
     page.add_field({
         label: 'Company',
         fieldtype: 'Link',
@@ -19,87 +35,112 @@ frappe.pages['balance-sheet-page-r'].on_page_load = function(wrapper) {
         reqd: 1,
         parent: filter_container,
         change: function() {
-            // Fetch all fiscal years instead of filtering by company
-            frappe.call({
-                method: "frappe.client.get_list",
-                args: {
-                    doctype: "Fiscal Year",
-                    fields: ["name", "year_start_date", "year_end_date"],
-                    order_by: "year_start_date desc"
-                },
-                callback: function(r) {
-                    if (r.message) {
-                        let fiscal_year_field = page.fields_dict.fiscal_year;
-                        fiscal_year_field.df.options = r.message.map(fy => fy.name).join('\n');
-                        fiscal_year_field.refresh();
+            generate_report_on_change();
+        }
+    });
 
-                        // Set default to most recent fiscal year
-                        if (r.message.length > 0) {
-                            fiscal_year_field.set_value(r.message[0].name);
-                            generate_report_on_change();
-                        }
-                    }
-                }
-            });
+    // Add fiscal year field
+    page.add_field({
+        label: 'Fiscal Year',
+        fieldtype: 'Link',
+        fieldname: 'fiscal_year',
+        options: 'Fiscal Year',
+        reqd: 1,
+        parent: filter_container,
+        change: function() {
+            fetch_fiscal_year_dates();
+        }
+    });
+
+    // Add date range fields (Initially Hidden)
+    page.add_field({
+        label: 'From Date',
+        fieldtype: 'Date',
+        fieldname: 'from_date',
+        reqd: 1,
+        parent: filter_container,
+        hidden: true,
+        change: function() {
+            generate_report_on_change();
         }
     });
 
     page.add_field({
-        label: 'Fiscal Year',
-        fieldtype: 'Select',
-        fieldname: 'fiscal_year',
-        options: [], // Will be populated dynamically
+        label: 'To Date',
+        fieldtype: 'Date',
+        fieldname: 'to_date',
         reqd: 1,
         parent: filter_container,
-        change: generate_report_on_change
+        hidden: true,
+        change: function() {
+            generate_report_on_change();
+        }
     });
 
+    // Add Accumulated Values Checkbox
     page.add_field({
-        label: 'Periodicity',
-        fieldtype: 'Select',
-        fieldname: 'periodicity',
-        options: [
-            'Yearly',
-            'Half Yearly',
-            'Quarterly',
-            'Monthly'
-        ],
-        default: 'Yearly',
-        reqd: 1,
+        label: 'Accumulated Values',
+        fieldtype: 'Check',
+        fieldname: 'accumulated_values',
+        default: 0,
         parent: filter_container,
-        change: generate_report_on_change
+        change: function() {
+            generate_report_on_change();
+        }
     });
 
-    function generate_report_on_change() {
-        let company = page.fields_dict.company.get_value();
-        let fiscal_year = page.fields_dict.fiscal_year.get_value();
-        let periodicity = page.fields_dict.periodicity.get_value();
+    function toggleFilterFields() {
+        const filterType = page.fields_dict.filter_type.get_value();
+        const isFiscalYear = filterType === 'fiscal_year';
 
-        if (!company || !fiscal_year) return;
+        // Toggle fields
+        page.fields_dict.fiscal_year.toggle(isFiscalYear);
+        page.fields_dict.from_date.toggle(!isFiscalYear);
+        page.fields_dict.to_date.toggle(!isFiscalYear);
 
-        // Get fiscal year dates
-        frappe.call({
-            method: "frappe.client.get",
-            args: {
-                doctype: "Fiscal Year",
-                name: fiscal_year,
-                fields: ["year_start_date", "year_end_date"]
-            },
-            callback: function(r) {
-                if (r.message) {
-                    generate_report(page, report_container, 
-                        r.message.year_start_date, 
-                        r.message.year_end_date
-                    );
-                }
-            }
-        });
+        // Clear values of hidden fields
+        if (isFiscalYear) {
+            page.fields_dict.from_date.set_value('');
+            page.fields_dict.to_date.set_value('');
+        } else {
+            page.fields_dict.fiscal_year.set_value('');
+        }
+
+        generate_report_on_change();
     }
 
-    function generate_report(page, report_container, start_date, end_date) {
-        let company = page.fields_dict.company.get_value();
-        let periodicity = page.fields_dict.periodicity.get_value();
+    function fetch_fiscal_year_dates() {
+        const fiscal_year = page.fields_dict.fiscal_year.get_value();
+        if (fiscal_year) {
+            frappe.db.get_doc('Fiscal Year', fiscal_year).then(doc => {
+                page.fields_dict.from_date.set_value(doc.year_start_date);
+                page.fields_dict.to_date.set_value(doc.year_end_date);
+                generate_report_on_change();
+            });
+        }
+    }
 
+    function generate_report_on_change() {
+        const filterType = page.fields_dict.filter_type.get_value();
+        let start_date, end_date;
+
+        if (filterType === 'fiscal_year') {
+            start_date = page.fields_dict.from_date.get_value();
+            end_date = page.fields_dict.to_date.get_value();
+        } else {
+            start_date = page.fields_dict.from_date.get_value();
+            end_date = page.fields_dict.to_date.get_value();
+        }
+
+        if (start_date && end_date) {
+            generate_report(report_container, start_date, end_date);
+        }
+    }
+
+    function generate_report(report_container, start_date, end_date) {
+        let company = page.fields_dict.company.get_value();
+        let accumulated_values = page.fields_dict.accumulated_values.get_value() ? 1 : 0;
+        
         frappe.call({
             method: "frappe.desk.query_report.run",
             args: {
@@ -109,11 +150,11 @@ frappe.pages['balance-sheet-page-r'].on_page_load = function(wrapper) {
                     "filter_based_on": "Date Range",
                     "period_start_date": start_date,
                     "period_end_date": end_date,
-                    "periodicity": periodicity,
+                    "periodicity": "Monthly",
                     "cost_center": [],
                     "project": [],
                     "selected_view": "Report",
-                    "accumulated_values": 1,
+                    "accumulated_values": accumulated_values,  // Set based on checkbox value
                     "include_default_book_entries": 1
                 }
             },
@@ -121,19 +162,16 @@ frappe.pages['balance-sheet-page-r'].on_page_load = function(wrapper) {
                 var data = response.message;
                 console.log("data.result", data.result);
 
-                let { firstArray: assets, secondArray: liabilities, thirdArray: totals } = filter_report(data.result);
+                let { firstArray: assets, secondArray: liabilities, thirdArray: equity, fourthArray: totals } = filter_report(data.result);
                 console.log('assets', assets);
 
-                // Clear previous report content
                 report_container.empty();
-
-                // Render template in report container
                 var html = frappe.render_template("balance_sheet_page_r", {
                     'assets': assets,
                     'liabilities': liabilities,
+                    'equity': equity,
                     'totals': totals
                 });
-                
                 report_container.html(html);
             }
         });
@@ -149,13 +187,21 @@ frappe.pages['balance-sheet-page-r'].on_page_load = function(wrapper) {
         }
         
         const firstArray = report.slice(0, indexes[0]);
-        const secondArray = report.slice(indexes[0], indexes[1]);
-        const thirdArray = report.slice(indexes[1]);
-        
+        const secondArray = report.slice(indexes[0]+1, indexes[1]);
+        const thirdArray = report.slice(indexes[1] + 1, indexes[2]);
+        const fourthArray = report.slice(indexes[2]+1);
+
+        console.log(firstArray, 'firstArray');
+        console.log(secondArray, 'secondArray');
+        console.log(thirdArray, 'thirdArray');
+        console.log(fourthArray, 'fourthArray');
+
         return {
             firstArray,
             secondArray,
             thirdArray
         };
     }
+
+    toggleFilterFields();
 };
